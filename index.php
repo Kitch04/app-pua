9d76ac9c3d938c14ca90954857ceb7530d8af441
Kitch04 <55270495+Kitch04@users.noreply.github.com>
2020-09-14 11:06:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <header>
+<link rel="stylesheet" href="style.css">
+</header>
+</head>
+<body>
+    <div class="logo"></div>
+</body>
+<footer><h2>Kitch Miyage O. Pua</h2></footer>
+</div>
+  
+</div>
+</html>
